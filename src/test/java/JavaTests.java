fe2d23aaa4f22eba47214f23b68bab00f7cb79bb
olegyablokov19 <oleg.yablokov19@gmail.com>
2024-09-10 12:21:26 +0300

    linux vpn

--- src/test/java/JavaTests.java
+++ src/test/java/JavaTests.java
@@ -1,3 +1,4 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,13 +9,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.sql.DriverManager;
+
 public class JavaTests {
     WebDriver driver;
 
     @BeforeEach
     public void setUp() {
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.manage().window().maximize();
+//        driver.manage().window().maximize();
     }
 
 //    @Test
