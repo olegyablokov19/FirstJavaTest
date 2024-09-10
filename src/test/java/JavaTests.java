89924ebb501c8f45e48c6fa33a1ea39b924dc3a2
olegyablokov19 <oleg.yablokov19@gmail.com>
2024-09-10 11:02:24 +0300

    linux vpn

--- src/test/java/JavaTests.java
+++ src/test/java/JavaTests.java
@@ -17,22 +17,49 @@ public class JavaTests {
         driver.manage().window().maximize();
     }
 
+//    @Test
+//    public void firstTest() {
+//        driver.get("https://google.com");
+//
+//        By fieldLocator = By.id("APjFqb");
+//        WebElement field = driver.findElement(fieldLocator);
+//
+//        field.sendKeys("автоматизированное тестирование");
+//        field.sendKeys(Keys.ENTER);
+//
+//        By articleLinkLocator = By.xpath("//a[*//span[text()='Википедия']]");
+//        WebElement articleLink = driver.findElement(articleLinkLocator);
+//        articleLink.click();
+//
+//        Assertions.assertEquals("https://ru.wikipedia.org/wiki/%D0%90%D0%B2%D1%82%D0%BE%D0%BC%D0%B0%D1%82%D0%B8%D0%B7%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5_%D1%82%D0%B5%D1%81%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5",
+//                driver.getCurrentUrl());
+//    }
+//    @Test
+//    public void secondTest() {
+//        driver.get("https://google.com");
+//
+//        By fieldLocator = By.id("APjFqb");
+//        WebElement field = driver.findElement(fieldLocator);
+//
+//        field.sendKeys("автоматизированное тестирование");
+//        field.sendKeys(Keys.ENTER);
+//
+//        By articleLinkLocator = By.xpath("//a[*//span[text()='Википедия']]");
+//        WebElement articleLink = driver.findElement(articleLinkLocator);
+//        articleLink.click();
+//
+//        Assertions.assertEquals("https://ru.wikipedia.orgD0%90%D0%B2%D1%82%D0%BE%D0%BC%D0%B0%D1%82%D0%B8%D0%B7%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5_%D1%82%D0%B5%D1%81%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5",
+//                driver.getCurrentUrl());
+//    }
+
     @Test
-    public void firstTest() {
-        driver.get("https://google.com");
-
-        By fieldLocator = By.id("APjFqb");
-        WebElement field = driver.findElement(fieldLocator);
-
-        field.sendKeys("автоматизированное тестирование");
-        field.sendKeys(Keys.ENTER);
-
-        By articleLinkLocator = By.xpath("//a[*//span[text()='Википедия']]");
-        WebElement articleLink = driver.findElement(articleLinkLocator);
-        articleLink.click();
-
-        Assertions.assertEquals("https://ru.wikipedia.org/wiki/%D0%90%D0%B2%D1%82%D0%BE%D0%BC%D0%B0%D1%82%D0%B8%D0%B7%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D0%BE%D0%B5_%D1%82%D0%B5%D1%81%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5",
-                driver.getCurrentUrl());
+    public void goToSosiski() {
+        driver.get("http://notify_stage.local.sosiska.ru/");
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @AfterEach
